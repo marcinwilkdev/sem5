e703a352009a49604cb34984a690fd8dc482316d
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-10-25 18:16:04 +0200

    Add some test data for jftt.

---
+++ jftt/lab2/zadanie2/test.py
@@ -0,0 +1,8 @@
+def main():
+    print("W not #a commentorks")  # this is another comment
+    print("W not #a commentorks")  # this is "another comment"
+    # this is a comment
+
+
+if __name__ == "__main__":
+    main()
