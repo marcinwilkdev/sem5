f3f9c24913ccdbbfe324fb6a21a336c320a67114
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-12-07 21:35:01 +0100

    Done lab4 scientific

--- naukowe/lab4/zadanie1_2_3_marcin_wilk.jl
+++ naukowe/lab4/zadanie1_2_3_marcin_wilk.jl
@@ -1,5 +1,7 @@
 module Algorytmy
 
+using Plots
+
 function ilorazyRoznicowe(x::Vector{Float64}, f::Vector{Float64})
     fCopy = [y for y in f]
     result = [fCopy[1]]
@@ -45,7 +47,29 @@ function naturalna(x::Vector{Float64}, fx::Vector{Float64})
     return a
 end
 
-function main()
+function rysujNnfx(f, a::Float64, b::Float64, n::Int)
+    h = (b - a) / n
+    xs = [a + k * h for k in 0:n]
+    ys = [f(x) for x in xs]
+    ilorazy = ilorazyRoznicowe(xs, ys)
+
+    n *= 100
+    h = (b - a) / n
+
+    printxs = [a + k * h for k in 0:n]
+    printys = [f(x) for x in printxs]
+    polynomialValues = [warNewton(xs, ilorazy, x) for x in printxs]
+
+    println(length(printxs))
+    println(length(printys))
+    println(length(polynomialValues))
+
+    plot(printxs, [printys, polynomialValues], label=["f(x)" "Nn(x)"])
+
+    savefig(string(a, b, n, "plot.png"))
+end
+
+function testFunctions()
     x = [3.0, 1.0, 5.0, 6.0]
     f = [1.0, -3.0, 2.0, 4.0]
     fx = ilorazyRoznicowe(x, f)
@@ -53,6 +77,25 @@ function main()
 
     println("Wartość wielomianu w punkcie 4.0: ", warNewton(x, fx, 4.0))
     println("Wartość wielomianu w punkcie 4.0: ", a[1] + a[2] * 4.0 + a[3] * 4.0^2 + a[4] * 4.0^3)
+end
+
+function main()
+    # testFunctions()
+    # rysujNnfx(x -> ℯ^x, 0.0, 1.0, 5)
+    # rysujNnfx(x -> ℯ^x, 0.0, 1.0, 10)
+    # rysujNnfx(x -> ℯ^x, 0.0, 1.0, 15)
+
+    # rysujNnfx(x -> x^2*sin(x), -1.0, 1.0, 5)
+    # rysujNnfx(x -> x^2*sin(x), -1.0, 1.0, 10)
+    # rysujNnfx(x -> x^2*sin(x), -1.0, 1.0, 15)
+
+    # rysujNnfx(x -> abs(x), -1.0, 1.0, 5)
+    # rysujNnfx(x -> abs(x), -1.0, 1.0, 10)
+    # rysujNnfx(x -> abs(x), -1.0, 1.0, 15)
+
+    rysujNnfx(x -> 1.0 / (1 + x^2), -5.0, 5.0, 5)
+    rysujNnfx(x -> 1.0 / (1 + x^2), -5.0, 5.0, 10)
+    rysujNnfx(x -> 1.0 / (1 + x^2), -5.0, 5.0, 15)
 end
 
 main()
